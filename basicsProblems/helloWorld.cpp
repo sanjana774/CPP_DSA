cd8b7947f429383ee9f03079cf4a7bd979b02ab5
sanjana774 <sanjana80840512@gmail.com>
2024-03-10 17:58:26 +0530

    basics of c++

--- basicsProblems/helloWorld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    // print hello world program in cpp//
-    cout<<"hello world in cpp";
-    return 0;
-}
---
+++ basicsProblems/hello_World.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //program to print hello world in cpp//
+    cout<<"hello World";
+    return 0;
+
+}
