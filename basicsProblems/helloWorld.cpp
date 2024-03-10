8ffc718eb5f873c38ba923682db7261ab9d1543a
sanjana774 <sanjana80840512@gmail.com>
2024-03-10 13:10:06 +0530

    First Commit

---
+++ basicsProblems/helloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // print hello world program in cpp//
+    cout<<"hello world in cpp";
+    return 0;
+}
---
+++ basicsProblems/swapInput.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+
+    cout<<"value of a is : "<<b<<endl;
+    cout<<"value of b is : "<<a;
+
+    return 0;
+}
