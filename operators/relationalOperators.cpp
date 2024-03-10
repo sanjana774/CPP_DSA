a644841ad09a857cc219ac701354d0b680efb674
sanjana774 <sanjana80840512@gmail.com>
2024-03-10 17:49:29 +0530

    relational operators

---
+++ operators/relationalOperators.cpp
@@ -0,0 +1,25 @@
+// IMPLEMENTAION OF RELATIONAL OPERATORS//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<< "First  number is :";
+    cin>>num1;
+
+    cout<< "Second  number is :";
+    cin>>num2;
+
+    cout<<"OUTPUT :"<<(num1==num2)<<endl;
+
+    cout<<"OUTPUT :"<<(num1!=num2)<<endl;
+
+    cout<<"OUTPUT :"<<(num1<=num2)<<endl;
+
+    cout<<"OUTPUT :"<<(num1>=num2)<<endl;
+
+    cout<<"OUTPUT :"<<(num1>num2)<<endl;
+
+    return 0;
+
+}
