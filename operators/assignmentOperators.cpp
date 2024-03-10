8a2c0f8ed5691faa974170a664fac14c8a8bbf3d
sanjana774 <sanjana80840512@gmail.com>
2024-03-10 18:38:41 +0530

    use of operators

---
+++ operators/assignmentOperators.cpp
@@ -0,0 +1,21 @@
+//implementation of assignment operators//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1;
+    int num2;
+
+    cout<<"1 number is :";
+    cin>>num1;
+    cout<<"2 number is :";
+    cin>>num2;
+
+    num1+=3;
+    cout<<num1<<endl;
+
+    num2-=5;
+    cout<<num2<<endl;
+
+    return 0;
+}
