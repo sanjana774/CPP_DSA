08143ea61a474b6d4c7d7584157a0ef174121ed4
sanjana774 <sanjana80840512@gmail.com>
2024-03-10 18:08:54 +0530

    OPERATORS

---
+++ operators/logicalOperators.cpp
@@ -0,0 +1,16 @@
+//implementation of logical operators//
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool exp1=true;
+    bool exp2=false;
+
+    cout<<(exp1&&exp2)<<endl;
+
+    cout<<(exp1||exp2)<<endl;
+
+    cout<<(!exp2)<<endl;
+
+    return 0;
+}
